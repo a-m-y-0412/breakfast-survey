220cc567e112a3c21d5419e14488474ea957f9b0
a-m-y-0412 <amyli0412@gmail.com>
2022-06-30 17:03:58 -0400

    removed everything but html

--- surveycode/index.js
+++
@@ -1,6 +0,0 @@
-// javascript
-// let breakItem = document.getElementById("breakfast-items")
-
-// function createMenu() {
-//     breakItem.textContent = "Tomorrow morning for breakfast, Amy will be having cereal with drink with side of "
-// }
