3711efe4bc53e07e921325a3923b53ad1512d33d
a-m-y-0412 <amyli0412@gmail.com>
2022-06-30 15:07:56 -0400

    Initialized project files
    Downloaded files from Scrimba and initialized this Repo with them. Moved package.json and webpack.config.js to .gitignore.

---
+++ surveycode/index.js
@@ -0,0 +1,6 @@
+// javascript
+// let breakItem = document.getElementById("breakfast-items")
+
+// function createMenu() {
+//     breakItem.textContent = "Tomorrow morning for breakfast, Amy will be having cereal with drink with side of "
+// }
